c05279736d3c226ad5ee0fea7b924a3558818358
jon hourcade <jonhourcade@gmail.com>
2021-07-07 08:48:29 -0500

    1:32:03

---
+++ functions/2-basic-api.js
@@ -0,0 +1,8 @@
+const items = require('../assets/data');
+
+module.exports.handler = async (event, context, callback) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(items),
+  };
+};
