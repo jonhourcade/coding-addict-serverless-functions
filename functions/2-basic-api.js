a3d416a35be2456f18666c6147412ad5a4d1c6ff
jon hourcade <jonhourcade@gmail.com>
2021-07-08 08:38:07 -0500

    testing deployment

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -3,6 +3,6 @@ const items = require('../assets/data');
 module.exports.handler = async (event, context, callback) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(items),
+    body: JSON.stringify(items)
   };
 };
